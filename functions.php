9c4e45e307f07b0dc3353074c4d9d5ae64436474
Phil Boothman <philboothman90@gmail.com>
2020-09-08 12:36:40 +0100

    Added Theme Support to functions.php

--- functions.php
+++ functions.php
@@ -10,3 +10,14 @@ function pb_setup() {
 }
 
 add_action('wp_enqueue_scripts', 'pb_setup');
+
+// Adding Theme Support
+
+function pb_init() {
+    add_theme_support('post-thumbnails');
+    add_theme_support('title-tag');
+    add_theme_support('html5',
+        array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
+}
+
+add_action('after_setup_theme', 'pb_init');
