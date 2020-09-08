4cc8b515d21e9c5d978631dc2e41fc871cfbc4e7
Phil Boothman <philboothman90@gmail.com>
2020-09-08 12:48:38 +0100

    Removed Material Icons dependency

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@
 
 function pb_setup() {
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;1,400&display=swap');
-    wp_enqueue_style('materialize-icons', '//fonts.googleapis.com/icon?family=Material+Icons');
     wp_enqueue_style('style', get_stylesheet_uri());
     wp_enqueue_script('main', get_theme_file_uri('/js/main.js'), NULL, '1.0', true);
 }
