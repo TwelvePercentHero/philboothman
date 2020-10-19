844d5b4a6600322fda9116447c942b68656c89ef
Phil Boothman <philboothman90@gmail.com>
2020-10-19 10:33:14 +0100

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<h2>Oh no...</h2>
+
+<p>There's no page here. Where is the page? No idea, but try one of the following links to get to where you need to go.</p>
+
+<ul>
+    <li><a href="<?php echo site_url('/about') ?>">About</a></li>
+    <li><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
+    <li><a href="<?php echo site_url('/contact') ?>">Contact</a></li>
+</ul>
+
+<?php get_footer(); ?>
